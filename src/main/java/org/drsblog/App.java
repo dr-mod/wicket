ab5e40f08076cf12666360d3ad05104e93d01f02
Dmitriy Panin <d.panin@levi9.com>
2014-12-02 11:33:47 +0200

    Created maven architype

---
+++ src/main/java/org/drsblog/App.java
@@ -0,0 +1,13 @@
+package org.drsblog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
