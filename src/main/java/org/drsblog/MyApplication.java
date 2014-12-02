8d0fdd4bddbebe9e402b48f1acd84752dd499529
Dmitriy Panin <d.panin@levi9.com>
2014-12-02 17:51:21 +0200

    Removed plugin; Added url to the page.

--- src/main/java/org/drsblog/MyApplication.java
+++ src/main/java/org/drsblog/MyApplication.java
@@ -2,11 +2,18 @@ package org.drsblog;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.target.coding.QueryStringUrlCodingStrategy;
 
 /**
  * Created by d.panin on 12/2/2014.
  */
 public class MyApplication extends WebApplication {
+
+    @Override
+    protected void init() {
+        super.init();
+        mount(new QueryStringUrlCodingStrategy("result", Hello.class));
+    }
 
     @Override
     public Class<? extends Page> getHomePage() {
